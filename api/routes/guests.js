7bac288b7a39e329cb734e866349600c707ad32e
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2017-10-14 12:10:21 +0200

    setting up get request for single guest

--- api/routes/guests.js
+++ api/routes/guests.js
@@ -1,48 +1,82 @@
-const express = require("express");
-const router = express.Router(); // get an instance of the express Router
-const MongoClient = require("mongodb").MongoClient;
 import { view, lensPath, lensIndex } from "ramda";
+import express from "express";
+import { ObjectID } from "mongodb";
+
+const router = express.Router();
+
+const GUEST_COLLECTION = "guests";
 
 const guestModel = {
   guests: "",
   attending: true,
-  songs: ""
+  songs: "",
+  email: "test@gmail.com"
 };
 
 const guestsLens = lensPath(["body", "guests"]);
 const attendingLens = lensPath(["body", "attending"]);
 const songsLens = lensPath(["body", "songs"]);
+const emailLens = lensPath(["body", "songs"]);
 
 module.exports = function(db) {
   router
     .route("/")
     //////////////////////////////////////////
-    // POST
+    // POST REQUEST
     //////////////////////////////////////////
     .post((request, response) => {
       const newGuest = Object.assign({}, guestModel, {
         guests: view(guestsLens, request),
         attending: view(attendingLens, request),
-        songs: view(songsLens, request)
+        songs: view(songsLens, request),
+        email: view(emailLens, request)
       });
 
-      db.collection("guests").insertOne(newGuest,
-
-      (err, doc) => {
+      db.collection(GUEST_COLLECTION).insertOne(newGuest, (err, doc) => {
         if (err) {
+          response.json({
+            error: "error in post request"
+          });
           return console.log(err, "error in post request");
         }
         response.status(201).json(view(lensIndex(0), doc.ops));
       });
     })
     //////////////////////////////////////////
-    // GET
+    // GET REQUEST FOR ALL GUESTS
     //////////////////////////////////////////
-    .get((request, res) => {
-      res.json({
-        svar: "funkar!"
-      });
+    .get((request, response) => {
+      db
+        .collection(GUEST_COLLECTION)
+        .find({})
+        .toArray(function(err, docs) {
+          if (err) {
+            response.json({
+              error: "error in get request for all guests"
+            });
+            return console.log(err, "error in get request");
+          } else {
+            response.status(200).json(docs);
+          }
+        });
     });
+  //////////////////////////////////////////
+  // GET REQUEST FOR SINGLE GUESTS
+  //////////////////////////////////////////
+  router.route("/:guest_id").get((request, response) => {
+    db
+      .collection(GUEST_COLLECTION)
+      .findOne({ _id: new ObjectID(request.params.guest_id) }, (err, doc) => {
+        if (err) {
+          response.json({
+            error: "error in get request for single guests"
+          });
+          return console.log(err, "error in get single guest request");
+        } else {
+          response.status(200).json(doc);
+        }
+      });
+  });
 
   return router;
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -11,27 +11,3 @@ module.exports = function(db) {
 
   return routes;
 };
-
-// const MongoClient = require("mongodb").MongoClient;
-//
-// const MONGO_URL = process.env.MONGODB_URI;
-//
-// MongoClient.connect(MONGO_URL, (err, db) => {
-//   if (err) {
-//     return console.log(err);
-//   }
-//
-//   // Do something with db here, like inserting a record
-//   db.collection("guests").insertOne({
-//     guests: "Hello MongoDB",
-//     text: "Hopefully this works!"
-//   },
-//   function(err, res) {
-//     if (err) {
-//       db.close();
-//       return console.log(err);
-//     }
-//     // Success
-//     db.close();
-//   });
-// });
