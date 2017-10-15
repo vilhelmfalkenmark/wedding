c04bfcba7affd272de372ee23cc138047efb1ffe
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2017-10-15 18:04:17 +0200

    implemented simple sass solution

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,16 @@ import thunk from "redux-thunk";
 import promise from "redux-promise-middleware";
 import reducer from "../reducers";
 
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const middleware = applyMiddleware(promise(), thunk);
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  middleware
-);
+let store;
+
+if (process.env.NODE_ENV === "development") {
+  store = createStore(reducer, devTools, middleware);
+} else {
+  store = createStore(reducer, middleware);
+}
 
 export default store;
